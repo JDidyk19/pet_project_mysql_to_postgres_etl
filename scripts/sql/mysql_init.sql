cbd0772381037bc6e48187578a8d92d4a03dc242
Yurii Didyk <juran2201@gmail.com>
2026-03-31 17:56:28 +0300

    Edit docker-compose.yml and mysql_init.sql
    - Added two services (generator and mysql)
    generator is built from Dockerfile which generate fake data
    mysql - it is a MYSQL database

--- scripts/sql/mysql_init.sql
+++ scripts/sql/mysql_init.sql
@@ -1,4 +1,52 @@
-LOAD DATA INFILE '/data/fake_data.csv'
+USE source_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    person_id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100) NULL,
+    gender CHAR(1) NOT NULL,
+    birth_date DATE NOT NULL,
+    age TINYINT UNSIGNED NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    salary_usd DECIMAL(10,2) NOT NULL,
+    is_employed BOOLEAN NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL
+);
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/data/users.csv'
 INTO TABLE users
+CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
-IGNORE 1 ROWS;
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(
+    person_id,
+    first_name,
+    last_name,
+    @middle_name,
+    gender,
+    birth_date,
+    age,
+    email,
+    phone_number,
+    country,
+    city,
+    company_name,
+    salary_usd,
+    @is_employed,
+    created_at,
+    updated_at
+)
+SET
+    middle_name = NULLIF(@middle_name, ''),
+    is_employed = CASE
+        WHEN @is_employed = 'True' THEN 1
+        ELSE 0
+    END;
